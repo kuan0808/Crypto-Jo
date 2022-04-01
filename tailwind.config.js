e15ee13004156a83d129033b871add402775f9fc
Kuan Ting Pan <kuanthedev@gmail.com>
2022-04-01 17:04:01 +0800

    ADD: custom colors and variables 'jo'

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        jo: {
+          "100": "#C8DFC0",
+          "200": "#7CAE65",
+          "300": "#629A46",
+          "400": "#4D7F32",
+          "500": "#3B702E",
+          "600": "#2E602A",
+          "700": "#2542EF",
+          "800": "#1B2B19",
+        },
+      },
+    },
   },
   plugins: [],
 };
